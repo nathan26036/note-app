e3ec870cb9e281fe4ce858c644b9f11820f3a33a
nathan26036 <nathandickinson04@gmail.com>
2024-01-11 01:48:30 -0500

    added server.js file that starts the server and creates routes for public file, created assetsRoute.js that gets the html files from public, created notesRoute.js file that post the notes title and text and also pushes the users input to the db.json file and also creates unique ids for the notes using uuid

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# note-app
+# Note Taker
+
+The goal is to create a notes app. The user is shown a start screen that has a button that brings up the main notes app. The user can then insert a note title and text that can be saved or cleared if the user changes their mind. Once the note app is saved then it will appear on the left where it can be reopened.
+
+## The process
+
+### What i did
+* Created a server.js file that uses express to create routes for the public folder
+* The server.js file also calls the routes to be used and starts the server
+* Created html.js file that gets both the html files
+* Created notes.js file that uses uuid to create a random id for each of the notes
+* Notes.js also post the title and text that the user inputs
+* Notes.js also pushes the users input to the db.json file to be saved
+
+![Notes app main page with test notes saved]()
+  
+## Installation 
+The final code tutorial can be found at 
---
+++ routes/notesRoute.js
@@ -0,0 +1,37 @@
+// dependencies 
+const path = require('path');
+const fs = require('fs')
+
+// npm package that allows for unique ids to be created
+const { v4: uuidv4 } = require('uuid');
+
+
+// routing
+module.exports = (app) => {
+
+  // GET /api/notes should read the db.json file and return all saved notes as JSON.
+  app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../db/db.json'));
+  });
+
+  // POST /api/notes should receive a new note to save on the request body, 
+  // add it to the db.json file, and then return the new note to the client. 
+  app.post('/api/notes', (req, res) => {
+    let db = fs.readFileSync('db/db.json');
+    db = JSON.parse(db);
+    res.json(db);
+    // creating body for note
+    let userNote = {
+      title: req.body.title,
+      text: req.body.text,
+      // creating unique id for each note
+      id: uuidv4(),
+    };
+    // pushing created note to be written in the db.json file
+    db.push(userNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(db));
+    res.json(db);
+
+  });
+
+};
